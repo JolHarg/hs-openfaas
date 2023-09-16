88b3dc795514f6f6aa7deffc3b104106bb350337
Dan Dart <git@dandart.co.uk>
2023-09-16 01:45:18 +0100

    proper x86_64?

--- default.nix
+++ default.nix
@@ -7,11 +7,12 @@
   compiler ? "ghc94"
 }:
 let
-  pkgsToUse = if builtins.currentSystem == "x86_64-linux" then nixpkgs else nixpkgs.pkgsCross.gnu64;
+  pkgsForX86 = if builtins.currentSystem == "x86_64-linux" then nixpkgs else nixpkgs.pkgsCross.gnu64.pkgsBuildHost;
+  pkgsX86 = if builtins.currentSystem == "x86_64-linux" then nixpkgs else nixpkgs.pkgsCross.gnu64.pkgsHostHost;
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   tools = haskell-tools compiler;
-  lib = pkgsToUse.pkgsBuildHost.haskell.lib;
-  myHaskellPackages = pkgsToUse.pkgsBuildHost.haskell.packages.${compiler}.override {
+  lib = pkgsX86.haskell.lib;
+  myHaskellPackages = pkgsX86.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       openfaas = lib.dontHaddock (self.callCabal2nix "openfaas" (gitignore ./.) {});
       # Tests for aeson don't work because they should be run as host
@@ -34,13 +35,17 @@ let
               rm -rf $PACKAGE/openfaas
               cp build/bin/openfaas $PACKAGE/openfaas
               rm -rf $PACKAGE/*.so*
-              cp ${pkgsToUse.pkgsHostHost.libffi.outPath}/lib64/libffi.so.8.1.2 $PACKAGE/libffi.so.8
-              cp ${pkgsToUse.pkgsHostHost.gmp.outPath}/lib/libgmp.so.10.5.0 $PACKAGE/libgmp.so.10
-              cp ${pkgsToUse.pkgsHostHost.glibc.outPath}/lib/{libc.so.6,libm.so.6,librt.so.1,libdl.so.2,ld-linux-x86-64.so.2} $PACKAGE/
+              cp ${pkgsX86.libffi.outPath}/lib64/libffi.so.8.1.2 $PACKAGE/libffi.so.8
+              cp ${pkgsX86.gmp.outPath}/lib/libgmp.so.10.5.0 $PACKAGE/libgmp.so.10
+              cp ${pkgsX86.glibc.outPath}/lib/{libc.so.6,libm.so.6,librt.so.1,libdl.so.2,ld-linux-x86-64.so.2} $PACKAGE/
               #x86_64-unknown-linux-gnu-strip $PACKAGE/openfaas
               chmod +w $PACKAGE/*
               patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $PACKAGE/libc.so.6
           done
+      }
+
+      clean() {
+        git clean -Xf packages
       }
 
       [[ -f packages/openfaas/debug/libc.so.6 ]] || build
@@ -53,19 +58,19 @@ let
         nodejs_20
         closurecompiler
         cabal-install
-        pkgsToUse.pkgsBuildHost.gcc
-        pkgsToUse.pkgsHostHost.gmp
-        pkgsToUse.pkgsHostHost.libffi
-        pkgsToUse.pkgsHostHost.glibc
+        pkgsForX86.gcc
+        pkgsX86.gmp
+        pkgsX86.libffi
+        pkgsX86.glibc
     ]);
     nativeBuildInputs = tools.defaultBuildTools ++ (with nixpkgs; [
         nodejs_20
         closurecompiler
         cabal-install
-        pkgsToUse.pkgsBuildHost.gcc
-        pkgsToUse.pkgsHostHost.gmp
-        pkgsToUse.pkgsHostHost.libffi
-        pkgsToUse.pkgsHostHost.glibc
+        pkgsForX86.gcc
+        pkgsX86.gmp
+        pkgsX86.libffi
+        pkgsX86.glibc
     ]);
     withHoogle = false;
   };
