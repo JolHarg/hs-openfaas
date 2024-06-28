692d4732ad71a26f250f33151ef143276c519b99
Dan Dart <git@dandart.co.uk>
2024-06-28 17:15:12 +0100

    Upgrade to ghc 9.8

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
     nixpkgs = nixpkgs;
     compiler = compiler;
   },
-  compiler ? "ghc96"
+  compiler ? "ghc98"
 }:
 let
   pkgsForX86 = if builtins.currentSystem == "x86_64-linux" then nixpkgs else nixpkgs.pkgsCross.gnu64.pkgsBuildHost;
