eaf5c938a85e2bb9a5b124c77cf19fcbca17e18f
Dan Dart <git@dandart.co.uk>
2025-06-08 22:39:28 +0100

    bulidables

--- shell.nix
+++ shell.nix
@@ -2,17 +2,17 @@ with import <nixpkgs> {};
 # needs mkShell in order to use headers/etc. from deps! how do we do that from nix-shell 
 mkShell rec {
     packages = [
-        pkg-config
-        zlib.dev
+        cabal-install
+        haskell.compiler.ghc912
+        # pkg-config
+        # zlib.dev
         krank
-        pkg-config
-        zlib.dev
-        pcre.dev
+        # pcre.dev
     ];
     shellHook = ''
         [[ -f ~/.local/bin/refactor ]] || cabal install apply-refact cabal-fmt doctest ghci-dap ghcid ghcide haskell-debug-adapter haskell-language-server hasktags hlint hoogle hpack implicit-hie stan stylish-haskell weeder --overwrite-policy=always --allow-newer
         export PATH=~/.local/bin:$PATH
         gen-hie > hie.yaml
-        for i in $(find -type f | grep -v dist-newstyle); do krank $i; done
+        for i in $(find -type f | grep -v "dist-*"); do krank $i; done
     '';
 }
